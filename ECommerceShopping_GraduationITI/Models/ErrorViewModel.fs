1875f144f0f2fc23b54a4ae0e493c5c5282bc7c0
Adel Hussein Abdelazeem <40367761+adelhussein97@users.noreply.github.com>
2023-02-10 22:57:43 +0200

    Delete ECommerceShopping_GraduationITI directory

--- ECommerceShopping_GraduationITI/Models/ErrorViewModel.fs
+++
@@ -1,8 +0,0 @@
-namespace ECommerceShopping_GraduationITI
-
-open System
-
-type ErrorViewModel private () =
-    member val RequestId : string = null with get, set
-
-    member val ShowRequestId : bool = true with get, set
