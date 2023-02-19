3c6f5c7826c5feed37e64b123747ee4c144c3d3a
Adel Hussein Abdelazeem <40367761+adelhussein97@users.noreply.github.com>
2023-02-19 22:18:01 +0200

    Create Graduation Project 2023

---
+++ GraduationProjectITI2023/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GraduationProjectITI2023.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
