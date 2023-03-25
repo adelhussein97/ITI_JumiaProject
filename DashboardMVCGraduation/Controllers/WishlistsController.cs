17d70868fed91572b2effe38c3ef4f8da3743b6e
adelhussein97 <adelhussein1579@gmail.com>
2023-03-26 01:22:27 +0200

    Create All Controllers of Domains

---
+++ DashboardMVCGraduation/Controllers/WishlistsController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DomainLayer;
+using DomainLayer.Models.Wishlists;
+
+namespace DashboardMVCGraduation.Controllers
+{
+    public class WishlistsController : Controller
+    {
+        private readonly EcommerceDbContext _context;
+
+        public WishlistsController(EcommerceDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Wishlists
+        public async Task<IActionResult> Index()
+        {
+            var ecommerceDbContext = _context.Wishlists.Include(w => w.Customer);
+            return View(await ecommerceDbContext.ToListAsync());
+        }
+
+        // GET: Wishlists/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Wishlists == null)
+            {
+                return NotFound();
+            }
+
+            var wishlist = await _context.Wishlists
+                .Include(w => w.Customer)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (wishlist == null)
+            {
+                return NotFound();
+            }
+
+            return View(wishlist);
+        }
+
+        // GET: Wishlists/Create
+        public IActionResult Create()
+        {
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "Id", "Id");
+            return View();
+        }
+
+        // POST: Wishlists/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,CustomerId,ProductId")] Wishlist wishlist)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(wishlist);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "Id", "Id", wishlist.CustomerId);
+            return View(wishlist);
+        }
+
+        // GET: Wishlists/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Wishlists == null)
+            {
+                return NotFound();
+            }
+
+            var wishlist = await _context.Wishlists.FindAsync(id);
+            if (wishlist == null)
+            {
+                return NotFound();
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "Id", "Id", wishlist.CustomerId);
+            return View(wishlist);
+        }
+
+        // POST: Wishlists/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,CustomerId,ProductId")] Wishlist wishlist)
+        {
+            if (id != wishlist.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(wishlist);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!WishlistExists(wishlist.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "Id", "Id", wishlist.CustomerId);
+            return View(wishlist);
+        }
+
+        // GET: Wishlists/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Wishlists == null)
+            {
+                return NotFound();
+            }
+
+            var wishlist = await _context.Wishlists
+                .Include(w => w.Customer)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (wishlist == null)
+            {
+                return NotFound();
+            }
+
+            return View(wishlist);
+        }
+
+        // POST: Wishlists/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Wishlists == null)
+            {
+                return Problem("Entity set 'EcommerceDbContext.Wishlists'  is null.");
+            }
+            var wishlist = await _context.Wishlists.FindAsync(id);
+            if (wishlist != null)
+            {
+                _context.Wishlists.Remove(wishlist);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool WishlistExists(int id)
+        {
+          return (_context.Wishlists?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
