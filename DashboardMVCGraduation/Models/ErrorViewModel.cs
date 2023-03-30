b09ddb0730391d2ddda85365631a4f75677220d6
Adel Hussein Abdelazeem <40367761+adelhussein97@users.noreply.github.com>
2023-03-30 02:06:35 +0200

    Delete DashboardMVCGraduation directory

--- DashboardMVCGraduation/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DashboardMVCGraduation.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
