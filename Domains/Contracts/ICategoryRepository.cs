87acb39cd51917dcefea2f9c4d559fd0debff1ee
KaramOsman <karam.Etman2998@gmail.com>
2023-03-30 17:05:14 +0200

    Create Crud Operations Of Category Entity and Create Designs

---
+++ Domains/Contracts/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using Domains;
+using WebApplication1.Models;
+
+namespace Domains.Contracts
+{
+    public interface ICategoryRepository : IRepository<Category, int>
+    {
+        Task<Category> FilterByAsync(string? filter = null);
+    }
+}
